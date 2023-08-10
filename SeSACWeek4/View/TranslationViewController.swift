5ee54cd9ffd002039351f4b9576e401934e59c41
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-08-11 00:46:41 +0900

    Feat: 언어 감지 API를 통해 langCode를 받아 번역 API 요청

--- SeSACWeek4/View/TranslationViewController.swift
+++ SeSACWeek4/View/TranslationViewController.swift
@@ -15,28 +15,75 @@ class TranslationViewController: UIViewController {
     @IBOutlet weak var translatedTextView: UITextView!
     @IBOutlet weak var translateButton: UIButton!
     
+    var langCode = "en"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        originalTextView.text = "입력"
+        originalTextView.text = "원하는 언어로 번역하고 싶은 문장을 입력하세요"
         translatedTextView.text = ""
         translatedTextView.isEditable = false
     }
     
     @IBAction func translateButtonClicked(_ sender: UIButton) {
+//        print("BEFORE: ", self.langCode)
+        getPapagoLanguageCode(query: originalTextView.text)
+//        print("AFTER: ", self.langCode)
+        getPapagoTranslationRequest(source: self.langCode)
+    }
+    
+    func getPapagoLanguageCode(query: String) {
+        let encodedText = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let url = "https://openapi.naver.com/v1/papago/detectLangs?query=\(encodedText)"
+        let headers: HTTPHeaders = [
+            "X-Naver-Client-Id": "\(APIKey.naverClientID)",
+            "X-Naver-Client-Secret": "\(APIKey.naverClientSecret)",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        ]
         
+        AF.request(url,
+                   method: .post,
+                   headers: headers
+        ).validate(statusCode: 200..<300
+        ).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let statusCode = response.response?.statusCode ?? 500
+                print("JSON: \(json)")
+                print("STATUS CODE: \(statusCode)")
+                
+                if statusCode == 200 {
+                    self.langCode = json["langCode"].stringValue
+                } else {
+                    print("REQUEST ERROR: \(statusCode)")
+                }
+
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getPapagoTranslationRequest(source: String) {
         let url = "https://openapi.naver.com/v1/papago/n2mt"
         let header: HTTPHeaders = [
             "X-Naver-Client-Id" : "\(APIKey.naverClientID)",
             "X-Naver-Client-Secret" : "\(APIKey.naverClientSecret)",
         ]
         let parameters: Parameters = [
-            "source": "ko",
+            "source": "\(source)",
             "target": "en",
             "text": originalTextView.text ?? "",
         ]
         
-        AF.request(url, method: .post, parameters: parameters, headers: header).validate().responseJSON { response in
+        AF.request(
+            url,
+            method: .post,
+            parameters: parameters,
+            headers: header
+        ).validate(
+        ).responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
@@ -50,5 +97,4 @@ class TranslationViewController: UIViewController {
             }
         }
     }
-    
 }
