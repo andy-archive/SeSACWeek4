da17dea9c95fddbb397a2c607bc8bdb8a2054e40
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-08-11 17:09:57 +0900

    Add: DispatchQueue의 main, global()를 이용하여 concurrent와 async 연습

---
+++ SeSACWeek4/View/AsyncViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AsyncViewController.swift
+//  SeSACWeek4
+//
+//  Created by Taekwon Lee on 2023/08/11.
+//
+
+import UIKit
+
+class AsyncViewController: UIViewController {
+
+    @IBOutlet weak var topImageView: UIImageView!
+    @IBOutlet weak var middleImageView: UIImageView!
+    @IBOutlet weak var bottomImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DispatchQueue.main.async { [self] in
+            configureView()
+        }
+    }
+    
+    func configureView() {
+        topImageView.backgroundColor = .black
+        topImageView.layer.cornerRadius = topImageView.frame.width / 2
+    }
+    
+    // sync(기다리기) async(안 기다리기 ) serial(메인 스레드 몰빵) concurrent(멀티 스레드 N빵)
+    // UI Freezing - 오래 걸리는 이벤트가 발생하는 동안 다른 것을 할 수 없다 (앱이 멈춘 상태)
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        guard let url = URL(string: "https://api.nasa.gov/assets/img/general/apod.jpg") else { return }
+        DispatchQueue.global().async { // 멀티 스레드에서 비동기로 url 작업 시키기
+            let data = try! Data(contentsOf: url)
+            
+            DispatchQueue.main.async { // 메인 스레드에서 비동기로 이미지에 대한 데이터 받아 오기
+                self.topImageView.image = UIImage(data: data)
+            }
+        }
+    }
+    
+
+}
